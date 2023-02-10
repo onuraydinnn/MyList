c0da38f3ae0a31482132b51050605ec3081cd5b7
onuraydinnn <onuraydin27@hotmail.com>
2023-02-10 16:06:28 +0300

    Add project files.

---
+++ MyList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Console.WriteLine(list.Count);
+            
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item) 
+        {
+            _tempArray= _array;
+                _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length-1] = item;
+        }
+
+        public int Count { get { return _array.Length; } }
+    }
+}
